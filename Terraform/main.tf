8193a4159835a06e3ba017b674745c3d155b60af
Jagvi Veluguleti <jagviveluguleti@gmail.com>
2025-10-16 14:00:22 +0530

    Latest now

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_iam_role_policy_attachment" "ecr_readonly_attach" {
 
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2RoleforSSM"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
